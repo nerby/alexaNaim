b8c9002b39eff1a9e5cf8a5c4c201af5b921f980
Atanu Ghosh <atanu@acm.org>
2017-06-11 19:25:26 -0700

    Add a function verify_msearch
    This function verifies all the mandatory fields and returns the search target and the maximum wait time.

--- upnp.py
+++ upnp.py
@@ -15,15 +15,16 @@ Atanu Ghosh
 from __future__ import print_function
 
 import socket
+import sys
 
-#  http://upnp.org/specs/arch/UPnP-arch-DeviceArchitecture-v1.0.pdf
+# http://upnp.org/specs/arch/UPnP-arch-DeviceArchitecture-v1.0.pdf
 
 MULTICAST_GRP = "239.255.255.250"
 MULTICAST_PORT = 1900
 
 def mlisten(grp, port):
     """
-    Listen for multicast packets and return them
+    Listen for a multicast packet and return it
     """
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
@@ -34,7 +35,8 @@ def mlisten(grp, port):
 
     sock.bind((grp, port))
     host = socket.gethostbyname(socket.gethostname())
-    sock.setsockopt(socket.SOL_IP, socket.IP_MULTICAST_IF, socket.inet_aton(host))
+    sock.setsockopt(socket.SOL_IP, socket.IP_MULTICAST_IF,
+                    socket.inet_aton(host))
     sock.setsockopt(socket.SOL_IP, socket.IP_ADD_MEMBERSHIP,
                     socket.inet_aton(grp) + socket.inet_aton(host))
 
@@ -45,6 +47,8 @@ def mlisten(grp, port):
         data, addr = sock.recvfrom(9000)
     except socket.error, exception:
         print('Exception', exception)
+
+    sock.close()
 
     return data, addr
 
@@ -68,6 +72,36 @@ def parse_upnp(data):
 
     return first_line[0], first_line[1], first_line[2], values
 
+def verify_msearch(data):
+    """
+    Verify this is a M-SEARCH packet
+    return the ST (Search target) and MX (Maximum wait time)
+    """
+
+    false_tuple = (False, None, None)
+
+    command, spec, version, values = parse_upnp(data)
+    if command != "M-SEARCH":
+        return false_tuple
+    if spec != "*":
+        return false_tuple
+    if version != "HTTP/1.1":
+        return false_tuple
+
+    mandatory_fields = ["HOST", "MAN", "MX", "ST"]
+    for i in mandatory_fields:
+        if not values.get(i):
+            return false_tuple
+
+    if values["HOST"] != "239.255.255.250:1900" and \
+        values["HOST"] != "239.255.255.250":
+        print("Unexpected value for HOST %s" % values["HOST"], file=sys.stderr)
+
+    if values["MAN"] != '"ssdp:discover"':
+        return false_tuple
+
+    return True, values["ST"], values["MX"]
+
 def main():
     """
     Main program sit in a loop printing packets
@@ -75,16 +109,24 @@ def main():
 
     while True:
         data, addr = mlisten(MULTICAST_GRP, MULTICAST_PORT)
-        print(addr)
+        try:
+            hostname = socket.gethostbyaddr(addr[0])
+        except socket.herror:
+            hostname = "UNKNOWN"
+        print(addr, hostname)
         print(data)
         command, spec, version, values = parse_upnp(data)
         print("Command", command)
         print("Spec", spec)
         print("Version", version)
         print("Values", values)
-        for i in values:
-            print(i, values[i])
-        
+        for key, value in values.iteritems():
+            print(key, value)
+
+        is_msearch, search_target, maximum_wait_time = verify_msearch(data)
+        if is_msearch:
+            print("M-SEARCH", search_target, maximum_wait_time)
+
         print('---')
 
 if __name__ == '__main__':
