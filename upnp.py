41fc87f94b88a67af3e52ab9cafcd765f9a5982e
Atanu Ghosh <atanu@acm.org>
2017-06-11 17:10:36 -0700

    Add a upnp parser

--- upnp.py
+++ upnp.py
@@ -48,6 +48,26 @@ def mlisten(grp, port):
 
     return data, addr
 
+def parse_upnp(data):
+    """
+    Parse packet the first line will be of the form
+    M-SEARCH * HTTP/1.1
+    The rest of the lines will be colon separated field names and values
+
+    return the first line as three fields and the other values as a dictionary
+    """
+
+    values = {}
+
+    lines = data.splitlines()
+    first_line = lines[0].split()
+    for i in lines[1:]:
+        pos = i.find(":")
+        if pos != -1:
+            values[i[0:pos]] = i[pos+1:].strip()
+
+    return first_line[0], first_line[1], first_line[2], values
+
 def main():
     """
     Main program sit in a loop printing packets
@@ -57,6 +77,14 @@ def main():
         data, addr = mlisten(MULTICAST_GRP, MULTICAST_PORT)
         print(addr)
         print(data)
+        command, spec, version, values = parse_upnp(data)
+        print("Command", command)
+        print("Spec", spec)
+        print("Version", version)
+        print("Values", values)
+        for i in values:
+            print(i, values[i])
+        
         print('---')
 
 if __name__ == '__main__':
